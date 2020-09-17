5c22c3aa4d8d736a0f081502ec86990cc4958607
DESKTOP-7E456UR\sjw <cider134@gmail.com>
2020-09-17 15:38:48 +0900

    POI 라이브러리를 이용하여 엑셀 다운로드

---
+++ excelDown/excelDown/src/main/java/com/example/demo/Application.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example")
+@MapperScan(basePackages="com.example.board.mapper")
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	// SqlSessionFactory 설정
+	@Bean
+	public SqlSessionFactory sqlSessoinFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		
+		sessionFactory.setDataSource(dataSource);
+		return sessionFactory.getObject();
+	}
+
+}
